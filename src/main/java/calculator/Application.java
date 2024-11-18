99e6a11e12d0b38ae8d283e40cefe497840cf863
이창근 <dlckdrms0517@gmail.com>
2024-11-19 01:02:05 +0900

    feat: 예외 발생시 콘솔에 예외를 출력하는 기능 개발

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,26 @@
 package calculator;
 
+import calculator.controller.CalculatorControllerExceptionHandleProxy;
 import calculator.controller.DefaultCalculatorController;
+import calculator.exception.ExceptionHandler;
 import calculator.io.input.InputHandler;
 import calculator.io.input.InputParser;
 import calculator.io.input.InputValidator;
 import calculator.io.output.OutputHandler;
 import calculator.io.reader.MissionUtilsReader;
 import calculator.io.writer.SystemWriter;
+import calculator.io.writer.Writer;
 
 public class Application {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler(new MissionUtilsReader(), new InputValidator(), new InputParser());
-        OutputHandler outputHandler = new OutputHandler(new SystemWriter());
+        Writer writer = new SystemWriter();
+        OutputHandler outputHandler = new OutputHandler(writer);
         DefaultCalculatorController defaultCalculatorController = new DefaultCalculatorController(inputHandler, outputHandler);
-        defaultCalculatorController.run();
+        CalculatorControllerExceptionHandleProxy calculatorControllerExceptionHandleProxy = new CalculatorControllerExceptionHandleProxy(
+                defaultCalculatorController,
+                new ExceptionHandler(writer)
+        );
+        calculatorControllerExceptionHandleProxy.run();
     }
 }
