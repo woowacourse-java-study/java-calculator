d3673801b3a1e472a4ce9f81dd87de2829a75ff8
023-dev <0_2_3@naver.com>
2024-11-20 00:54:25 +0900

    feat(constant): 구분자 로직에 대한 상수 값 추가

--- src/main/java/calculator/common/constant/Constants.java
+++ src/main/java/calculator/common/constant/Constants.java
@@ -2,9 +2,13 @@ package calculator.common.constant;
 
 public class Constants {
 
-    public static final String DEFAULT_DELIMITER_COMMA = ",";
-    public static final String DEFAULT_DELIMITER_COLON = ":";
+    public static final String EMPTY = "";
+    public static final String COMMA = ",";
+    public static final String COLON = ":";
+    public static final String PIPE = "|";
+    public static final String DEFAULT_DELIMITERS = String.join(PIPE, COMMA, COLON);
 
     public static final String CUSTOM_DELIMITER_PREFIX = "//";
     public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    public static final String CUSTOM_DELIMITER_PATTERN = "//(.+)\n(.*)";
 }
