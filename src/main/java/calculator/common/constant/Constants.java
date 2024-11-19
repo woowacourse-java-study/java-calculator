51ea5cbcbf945223ee5b3d55a6be840fe24880c0
023-dev <0_2_3@naver.com>
2024-11-20 03:04:40 +0900

    feat: 빈값 입력 처리

--- src/main/java/calculator/common/constant/Constants.java
+++ src/main/java/calculator/common/constant/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
     public static final String ZERO = "0";
     public static final String DEFAULT_DELIMITERS = String.join(PIPE, COMMA, COLON);
     public static final String DEFAULT_DELIMITER_PATTERN = "^[0-9]+([,|:][0-9]+)*$";
-    public static final String CUSTOM_DELIMITER_PATTERN = "^//.*\\\\n([0-9]+([,|:+][0-9]+)*)?$";
+    public static final String CUSTOM_DELIMITER_PATTERN = "^//.*\\\\n(.*)?$";
     public static final String CUSTOM_DELIMITER_PARSE_PATTERN = "//(.*)\\\\n(.*)";
 }
