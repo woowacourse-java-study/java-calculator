b1fbe2e88db048d018358d41b5297ae62d0d6f9e
023-dev <0_2_3@naver.com>
2024-11-19 21:26:24 +0900

    feat(message): View에 사용할 문자열 상수 관리 클래스 ViewMessages 추가

---
+++ src/main/java/calculator/common/constant/message/ViewMessages.java
@@ -0,0 +1,6 @@
+package calculator.common.constant.message;
+
+public class ViewMessages {
+    public static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해주요.";
+    public static final String CALCULATOR_RESULT_MESSAGE = "결과 : %d";
+}
