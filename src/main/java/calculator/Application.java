f231bbba944b5f1ba8dad11e83110ec18d89c435
023-dev <0_2_3@naver.com>
2024-11-20 01:15:57 +0900

    refactor(main): 시작점 main 메서드에서 초기화 로직 간소화 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,10 @@
 package calculator;
 
 import calculator.config.CalculatorConfig;
-import calculator.controller.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        CalculatorConfig config = new CalculatorConfig();
-        CalculatorController calculator = config.getCalculatorController();
-        calculator.run();
+        CalculatorConfig calculatorConfig = new CalculatorConfig();
+        calculatorConfig.run();
     }
 }
