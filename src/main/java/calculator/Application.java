333f918af34ba4b7a8a618e0ead16c9c468a8081
023-dev <0_2_3@naver.com>
2024-11-19 21:40:08 +0900

    feat(main): 시작점 main 메서드에 config 및 controller 초기화 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.config.CalculatorConfig;
+import calculator.controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorConfig config = new CalculatorConfig();
+        CalculatorController calculator = config.getCalculatorController();
+        calculator.run();
     }
 }
