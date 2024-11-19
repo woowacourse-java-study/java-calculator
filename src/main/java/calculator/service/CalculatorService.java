9e4cc54d14a270948fb8e3eaa748764455a0faf0
023-dev <0_2_3@naver.com>
2024-11-20 00:56:59 +0900

    feat(service): 구분자 처리 메서드 parse 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,9 +1,33 @@
 package calculator.service;
 
+import calculator.util.parser.NumberParser;
+import calculator.util.validator.InputValidator;
 import java.util.List;
 
 public class CalculatorService {
-    public static int sum(List<Integer> numbers) {
-        return numbers.stream().reduce(0, Integer::sum);
+    private final InputValidator inputValidator;
+    private final NumberParser numberParser;
+
+    public CalculatorService(InputValidator inputValidator, NumberParser numberParser) {
+        this.inputValidator = inputValidator;
+        this.numberParser = numberParser;
+    }
+
+    public int calculator(String input) {
+        validate(input);
+        List<Integer> numbers = parse(input);
+        return sum(numbers);
+    }
+
+    private void validate(String input) {
+        inputValidator.validate(input);
+    }
+
+    private List<Integer> parse(String input) {
+        return numberParser.parse(input);
+    }
+
+    private int sum(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).sum();
     }
 }
