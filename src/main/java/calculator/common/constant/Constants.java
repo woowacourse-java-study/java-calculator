7151248ca93466837a7b254d31321f4f60886f1f
023-dev <0_2_3@naver.com>
2024-11-19 21:49:52 +0900

    feat(constant): 상수 값 관리를 위한 클래스 Constants 추가

---
+++ src/main/java/calculator/common/constant/Constants.java
@@ -0,0 +1,10 @@
+package calculator.common.constant;
+
+public class Constants {
+
+    public static final String DEFAULT_DELIMITER_COMMA = ",";
+    public static final String DEFAULT_DELIMITER_COLON = ":";
+
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+}
