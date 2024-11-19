d6f833cfcdc68a692e6d5668437e4b92f6bcbb4d
023-dev <0_2_3@naver.com>
2024-11-20 01:19:49 +0900

    feat(message): 에러 메시지에 대한 문자열 상수 처리 클래스 ErrorMessages 추가

--- src/main/java/calculator/common/constant/message/ErrorMessages.java
+++ src/main/java/calculator/common/constant/message/ErrorMessages.java
@@ -2,6 +2,7 @@ package calculator.common.constant.message;
 
 public class ErrorMessages {
     public final static String INVALID_EMPTY = "입력 값은 비어 있을 수 없습니다.";
+    public final static String INVALID_INPUT_FORMAT = "입력 문자열이 유효한 형식이 아닙니다.";
     public final static String INVALID_CONTAIN_NUMBER = "입력 값에 숫자가 포함될 수 없습니다.";
     public final static String INVALID_NUMBER = "유효하지 않은 숫자입니다.";
 }
